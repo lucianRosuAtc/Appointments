ff9f666fc9d0eb8080e0a3b84f250114f77e5df4
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-10-02 19:07:32 +0100

    Add user creation functionality and Appwrite configuration

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 
 
@@ -42,11 +43,11 @@ const [isLoading, setIsLoading] = useState(false)
     setIsLoading(true)
 
     try{
-        // const userData = { name, email, phone}
+        const userData = { name, email, phone}
 
-        // const user = await createUser(userData)
+        const user = await createUser(userData)
 
-        // if(user) router.push(`/patients/${user.$id}/register`)
+        if(user) router.push(`/patients/${user.$id}/register`)
         
     }catch (error){
       console.log(error)
