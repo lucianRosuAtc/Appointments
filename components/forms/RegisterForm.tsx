83ac9e8f775cdaa342866de10348eded3e8e2ffa
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-10-11 17:01:36 +0100

    Add gender options constant and implement radio and select components

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Form } from "@/components/ui/form"
+import { Form, FormControl } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
@@ -11,61 +11,132 @@ import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
+import { RadioGroup } from "@radix-ui/react-radio-group"
+import { GenderOptions } from "@/constants"
+import { RadioGroupItem } from "../ui/radio-group"
+import { Label } from "../ui/label"
 
 
 
-const RegisterForm = ({user}:{user: User}) => {
-  const router = useRouter()
-const [isLoading, setIsLoading] = useState(false)
 
-  const form = useForm<z.infer<typeof UserFormValidation>>({
-    resolver: zodResolver(UserFormValidation),
-    defaultValues: {
-      name: "",
-      email: "",
-      phone: "",
-    },
-  })
+const RegisterForm = ({ user }: { user: User }) => {
+    const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+        },
+    })
 
 
-  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
-    setIsLoading(true)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
 
-    try{
-        const userData = { name, email, phone}
+        try {
+            const userData = { name, email, phone }
 
-        const user = await createUser(userData)
+            const user = await createUser(userData)
 
-        if(user) router.push(`/patients/${user.$id}/register`)
-        
-    }catch (error){
-      console.log(error)
+            if (user) router.push(`/patients/${user.$id}/register`)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
-  }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">Welcome to our clinic</h1>
-          <p className="text-neutral-400">Schedule your first appointment.</p>
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+                <section className="space-y-4">
+                    <h1 className="header">Welcome</h1>
+                    <p className="text-neutral-400">Let us know more about yourself</p>
+                </section>
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Personal Information</h2>
+                    </div>
+                </section>
 
-        </section>
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="name"
+                    label="Full Name"
+                    placeholder="Tommy Oliver"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="User Icon"
+                />
 
-      <CustomFormField 
-        fieldType={FormFieldType.INPUT}
-        control={form.control}
-        name="name"
-        label="Full Name"
-        placeholder="Tommy Oliver"
-        iconSrc= "/assets/icons/user.svg"
-        iconAlt="User Icon"
-      />
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="Tommy.Oliver@gmail.com"
+                        iconSrc="/assets/icons/email.svg"
+                        iconAlt="Email Icon"
+                    />
+                    <CustomFormField
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        control={form.control}
+                        name="phone"
+                        label="Phone Number"
+                        placeholder="(555) 123-4567"
+                    />
+                </div>
 
-        <SubmitButton className="" isLoading={ isLoading }>Get Started</SubmitButton>
-      </form>
-    </Form>
-  )
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.DATE_PICKER}
+                        control={form.control}
+                        name="birthDate"
+                        label="Date of Birth"
+
+                    />
+                    <CustomFormField
+                        fieldType={FormFieldType.SKELETON}
+                        control={form.control}
+                        name="gender"
+                        label="Gender"
+                        renderSkeleton={(field) => (
+                            <FormControl>
+                                <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange}
+                                    defaultValue={field.value}>
+                                    {GenderOptions.map((option)=>(
+                                        <div className="radio-group" key={option}>
+                                            <RadioGroupItem value={option} id={option} 
+                                            <Label>
+                                            {option}
+                                            </Label>
+                                            />
+                                        </div>
+                                    ))}
+                                </RadioGroup>
+                            </FormControl>
+                        )}
+                    />
+                </div>
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
+                <div className="flex flex-col gap-6 xl:flex-row">
+
+                </div>
+
+
+
+                <SubmitButton className="" isLoading={isLoading}>Get Started</SubmitButton>
+            </form>
+        </Form>
+    )
 }
 
 export default RegisterForm
